4c2688f4c62150e4aa445a98054221b0b655e4d5
Mrinal Mohit <mrinalmohit@fb.com>
2016-07-28 04:09:04 -0700

    initial stable commit

--- BreakoutGL/Headers/glitter.hpp
+++ BreakoutGL/Headers/glitter.hpp
@@ -47,7 +47,7 @@ GLFWwindow* setupWindowing(const char* windowName, int width, int height) {
     glEnable(GL_CULL_FACE);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-//    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+    // glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
     return window;
 }
